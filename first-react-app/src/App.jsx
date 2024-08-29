736356d701a3b70af44d80172f5d9245cd51c035
unknown <polibos6366@gmail.com>
2024-08-28 19:31:17 -0600

    non-string props

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -1,12 +1,18 @@
 import "./App.css";
 import Chicken from "./Chicken";
+import Greeter from "./Greeter";
+
+import Die from "./Die";
 
 function App() {
   return (
     <>
-      <Chicken />
-      <Chicken />
-      <Chicken />
+      {/* <Greeter person="Sarai" from="Jesus" />
+      <Greeter person="Natalia" from="Jesus" />
+      <Greeter person="Natalia" from="Sarai" /> */}
+      <Die numSides={20} />
+      <Die numSides={6} />
+      <Die numSides={10} />
     </>
   );
 }
---
+++ first-react-app/src/Die.jsx
@@ -0,0 +1,6 @@
+const Die = ({ numSides }) => {
+  const roll = Math.floor(Math.random() * numSides) + 1;
+  return <p>Die roll: {roll}</p>;
+};
+
+export default Die;
