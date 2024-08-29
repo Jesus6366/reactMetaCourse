a5a3f95aea7662efea84254b2ae699b6d832a22a
unknown <polibos6366@gmail.com>
2024-08-28 19:57:32 -0600

    react conditionals

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -5,18 +5,23 @@ import Greeter from "./Greeter";
 import Die from "./Die";
 
 import ListPicker from "./ListPicker";
+import DoubleDice from "./DoubleDice";
 
 function App() {
   return (
     <>
+      <DoubleDice />
+      <DoubleDice />
+      <DoubleDice />
+
       {/* <Greeter person="Sarai" from="Jesus" />
       <Greeter person="Natalia" from="Jesus" />
       <Greeter person="Natalia" from="Sarai" /> */}
       {/* <Die numSides={20} />
       <Die numSides={6} />
       <Die numSides={10} /> */}
-      <ListPicker values={[1, 2, 3]} />
-      <ListPicker values={["a", "b", "c"]} />
+      {/* <ListPicker values={[1, 2, 3]} />
+      <ListPicker values={["a", "b", "c"]} /> */}
     </>
   );
 }
