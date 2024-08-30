52f6a0ced58287ae0eda1433ef21b33383e17a59
unknown <polibos6366@gmail.com>
2024-08-29 19:22:20 -0600

    excersice 2 components

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -10,34 +10,21 @@ import DoubleDice from "./DoubleDice";
 import ColorList from "./ColorList";
 import Slots from "./Slots";
 import ShoppingList from "./ShoppingList";
+import PropertyList from "./PropertyList";
 
-const data = [
-  { item: "eggs", quantity: 12, completed: false },
-  { item: "milk", quantity: 1, completed: true },
-  { item: "chicken breasts", quantity: 4, completed: false },
-  { item: "carrots", quantity: 6, completed: true },
+const properties = [
+  { id: 129031, name: "Desert Yurt", rating: 4.9, price: 150 },
+  { id: 129331, name: "Lone Mountain Cabin", rating: 4.8, price: 250 },
+  { id: 129032, name: "Cactus Retreat", rating: 4.75, price: 300 },
+  { id: 129033, name: "Redwood Treehouse Escape", rating: 4.9, price: 120 },
+  { id: 129034, name: "Oceanview Condo", rating: 4.7, price: 140 },
+  { id: 129035, name: "Gold Miner Campground", rating: 4.69, price: 96 },
 ];
 
 function App() {
   return (
     <>
-      <ShoppingList items={data} />
-      {/* <Slots val1="🍒" val2="🍒" val3="🍒" />
-      <Slots val1="🍒" val2="🍌" val3="🍒" /> */}
-      {/* <DoubleDice />
-      <DoubleDice />
-      <DoubleDice />
-
-      <ColorList colors={["red", "pink", "purple", "teal"]} />
-      <ColorList colors={["orangeRed", "olive", "slategrey"]} /> */}
-      {/* <Greeter person="Sarai" from="Jesus" />
-      <Greeter person="Natalia" from="Jesus" />
-      <Greeter person="Natalia" from="Sarai" /> */}
-      {/* <Die numSides={20} />
-      <Die numSides={6} />
-      <Die numSides={10} /> */}
-      {/* <ListPicker values={[1, 2, 3]} />
-      <ListPicker values={["a", "b", "c"]} /> */}
+      <PropertyList properties={properties} />
     </>
   );
 }
