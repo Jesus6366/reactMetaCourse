c821c67c5ef537afc27b73525d131c2a2a4f6318
unknown <polibos6366@gmail.com>
2024-08-30 17:45:28 -0600

    random color boxes

--- first-react-app/src/App.jsx
+++ first-react-app/src/App.jsx
@@ -17,6 +17,8 @@ import Counter from "./Counter";
 
 import Toggler from "./Toggler";
 
+import ColorBox from "./ColorBox";
+
 const properties = [
   { id: 129031, name: "Desert Yurt", rating: 4.9, price: 150 },
   { id: 129331, name: "Lone Mountain Cabin", rating: 4.8, price: 250 },
@@ -29,7 +31,36 @@ const properties = [
 function App() {
   return (
     <>
-      <Toggler />
+      <ColorBox
+        colors={[
+          "#e46535",
+          "#9c27b0",
+          "#679ab7",
+          "#e53935",
+          "#54a9f4",
+          "#009688",
+          "#e5503b",
+          "#d953b4",
+          "#68a1c0",
+          "#e5492b",
+          "#4b8bf4",
+          "#00a79d",
+          "#e64a36",
+          "#a34e9b",
+          "#4a9ac7",
+          "#e54a2a",
+          "#009e8b",
+          "#e56036",
+          "#9c1b6d",
+          "#68a0b7",
+          "#e5512c",
+          "#00a99d",
+          "#e55137",
+          "#54a6f8",
+          "#00796b",
+        ]}
+      />
+      {/* <Toggler /> */}
       {/* {<Counter />} */}
       {/* <Clicker message="Please Stop Clicking Me!" buttonText="Do not click" /> */}
       {/* <PropertyList properties={properties} /> */}
