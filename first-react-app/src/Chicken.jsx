8ee8568e7d785f86639d89827f6dbcea329059b0
Jesus Jose Martha Laborin <89470786+Jesus6366@users.noreply.github.com>
2024-08-31 16:00:01 -0600

    Delete first-react-app directory

--- first-react-app/src/Chicken.jsx
+++
@@ -1,7 +0,0 @@
-import "./Chicken.css";
-
-const Chicken = () => {
-  return <div className="chicken">Bock Bock Bock</div>;
-};
-
-export default Chicken;
--- first-react-app/src/Die.jsx
+++
@@ -1,6 +0,0 @@
-const Die = ({ numSides = 6 }) => {
-  const roll = Math.floor(Math.random() * numSides) + 1;
-  return <p>Die roll: {roll}</p>;
-};
-
-export default Die;
